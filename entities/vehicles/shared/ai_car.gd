85c8f78436fa13c990840ed5b0fa3a5ee94a1480
lampealex888 <lampealex888@gmail.com>
2025-07-09 11:57:19 -0400

    Fix despawn timer

--- entities/vehicles/shared/ai_car.gd
+++ entities/vehicles/shared/ai_car.gd
@@ -4,19 +4,17 @@ signal path_end_reached
 
 const PATH_TRANSMITTER = 4
 @export var car_velocity: float = 2.0
-var despawn_time: float = 5.0
-var _despawn_timer: float = 0
 var _new_path
 var _is_following_path: bool = true
 
 @onready var _raycast = $RigidBody3D/RayCast3D
 @onready var _rigid_body = $RigidBody3D
 @onready var _area3d = $RigidBody3D/Area3D
+@onready var despawn_timer = $RigidBody3D/DespawnTimer
 
 func _ready():
-	if _area3d.has_signal("body_entered"):
-		print("signal_connected")
-		_area3d.body_entered.connect(_on_player_collision)
+	_area3d.body_entered.connect(_on_player_collision)
+	despawn_timer.timeout.connect(despawn)
 
 
 func _process(delta):
@@ -30,13 +28,6 @@ func _process(delta):
 		# Check if we've reached the end (progress_ratio goes from 0 to 1)
 		if progress_ratio >= 1.0:
 			_on_path_end_reached()
-	else:
-		_despawn_timer += delta
-		if _despawn_timer > despawn_time:
-			if _rigid_body and is_instance_valid(_rigid_body):
-				_rigid_body.queue_free()
-			# Remove the PathFollow3D node as well
-			queue_free()
 
 
 func _on_path_end_reached() -> void:
@@ -68,11 +59,11 @@ func _check_raycast_collisions():
 func _on_player_collision(body):
 	# If the player hits an ai car, detach from pathfollow3d and attach 
 	# to root as rigid body
-	print(body.name)
 	if body.is_in_group("player_car"):
 		if not _is_following_path:
 			return # Already detached
 		_is_following_path = false
+		despawn_timer.start()
 		var current_global_position = _rigid_body.global_position
 		var current_global_rotation = _rigid_body.global_rotation
 		var root = get_tree().current_scene
@@ -82,3 +73,9 @@ func _on_player_collision(body):
 		_rigid_body.global_rotation = current_global_rotation
 		_raycast.enabled = false
 		process_mode = Node.PROCESS_MODE_DISABLED
+
+func despawn():
+	if _rigid_body and is_instance_valid(_rigid_body):
+		_rigid_body.queue_free()
+		# Remove the PathFollow3D node as well
+		queue_free()
