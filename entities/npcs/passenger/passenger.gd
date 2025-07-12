ac2ac64f5aa31ba3a760bd40510c6effc7622251
lampealex888 <lampealex888@gmail.com>
2025-07-12 17:13:53 -0400

    Random passenger mesh
    Also split buildings into two scenes, one for model and one for gameplay elements

--- entities/npcs/passenger/passenger.gd
+++ entities/npcs/passenger/passenger.gd
@@ -3,15 +3,41 @@ extends CharacterBody3D
 var destination_building
 var current_building
 var fare: int = 50
-@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+
 @onready var destination_arrow: Node3D = $DestinationArrow
 @onready var pickup_area: Area3D = $PickupArea3D
-@onready var mesh = $"character-q2"
+@onready var collision_shape: CollisionShape3D = $CollisionShape3D
+
+var current_characater = null
+var character_paths = [
+	"res://entities/npcs/character_c/character_c.tscn",
+	"res://entities/npcs/character_b/character_b.tscn",
+	"res://entities/npcs/character_d/character_d.tscn",
+	"res://entities/npcs/character_e/character_e.tscn",
+	"res://entities/npcs/character_f/character_f.tscn",
+	"res://entities/npcs/character_g/character_g.tscn",
+	"res://entities/npcs/character_h/character_h.tscn",
+	"res://entities/npcs/character_i/character_i.tscn",
+	"res://entities/npcs/character_j/character_j.tscn",
+	"res://entities/npcs/character_k/character_k.tscn",
+	"res://entities/npcs/character_l/character_l.tscn",
+	"res://entities/npcs/character_m/character_m.tscn",
+	"res://entities/npcs/character_n/character_n.tscn",
+	"res://entities/npcs/character_o/character_o.tscn",
+	"res://entities/npcs/character_p/character_p.tscn",
+	"res://entities/npcs/character_q/character_q.tscn",
+	"res://entities/npcs/character_r/character_r.tscn"
+]
 
 signal destination_set(destination: String)
 
 func _ready():
 	pickup_area.body_entered.connect(_on_pickup_area_entered)
+	var random_char_path = character_paths[randi() % character_paths.size()]
+	var character_scene = load(random_char_path)
+	var random_character = character_scene.instantiate()
+	add_child(random_character)
+	current_characater = random_character
 
 
 func _on_pickup_area_entered(body):
@@ -21,7 +47,7 @@ func _on_pickup_area_entered(body):
 			if child.is_in_group("passengers"):
 				return
 		collision_shape.disabled = true
-		mesh.visible = false
+		current_characater.visible = false
 		pickup_area.visible = false
 		get_parent().call_deferred("remove_child", self)
 		body.call_deferred("add_child", self)
@@ -36,7 +62,6 @@ func _set_up_trip(body):
 		destination_arrow.activate(destination_building.global_position, self)
 		destination_arrow.visible = true
 	destination_set.emit(destination_building.building_name)
-	
 
 
 func dropoff_at_building(building) -> bool:
--- utilities/game/game_manager.gd
+++ utilities/game/game_manager.gd
@@ -4,13 +4,14 @@ extends Node
 const PICKUP_TIME_BONUS := 15.0
 const DELIVERY_TIME_BONUS := 10.0
 const GAME_DURATION := 90.0
-const BASE_FARE := 50.0
+const BASE_FARE := 50
 
 var total_score := 0
 var game_time_remaining := GAME_DURATION
 var is_game_active := false
 
 @onready var game_timer: Timer = $GameTimer
+@onready var update_timer: Timer = $UpdateTimer
 
 signal score_updated(new_score: int)
 signal timer_updated(time_remaining: float)
@@ -21,13 +22,15 @@ func _ready() -> void:
 	# Setup game timer
 	game_timer.wait_time = GAME_DURATION
 	game_timer.timeout.connect(_end_game)
+	update_timer.timeout.connect(_on_update_timer_timeout)
 	await get_tree().process_frame
 	var buildings = get_tree().get_nodes_in_group("buildings")
 	for building in buildings:
 		if building.has_signal("passenger_delivered"):
 			building.passenger_delivered.connect(_on_passenger_delivered)
 
-func _process(delta: float) -> void:
+
+func _on_update_timer_timeout() -> void:
 	if is_game_active:
 		game_time_remaining = game_timer.time_left
 		timer_updated.emit(game_time_remaining)
