19ff7d2d8b752f390ce2fe5db516097eea3fc0e5
lampealex888 <lampealex888@gmail.com>
2025-07-15 11:51:42 -0400

    Add headlights, brakelights

--- entities/vehicles/player_car/vehicle.gd
+++ entities/vehicles/player_car/vehicle.gd
@@ -7,10 +7,16 @@ const BRAKE_STRENGTH := 2.0
 @export var engine_force_value := 40.0
 
 var previous_speed := linear_velocity.length()
-var _steer_target := 0.0
+var steer_target := 0.0
 var disabled := false
+var brake_lights_on := false
 
+
+@onready var brake_light_left: MeshInstance3D = $BrakeLightLeftMesh
+@onready var brake_light_right: MeshInstance3D = $BrakeLightRightMesh
 @onready var desired_engine_pitch: float = $EngineSound.pitch_scale
+
+var brake_light_material: StandardMaterial3D
 
 func _ready():
 	await get_tree().process_frame
@@ -18,6 +24,10 @@ func _ready():
 	if game_manager:
 		game_manager.game_started.connect(enable_vehicle)
 		disabled = true
+	brake_light_material = brake_light_right.get_active_material(0)
+	brake_light_material.emissions_enabled = true
+	brake_light_material.emission = Color(0.941, 0.012, 0.196)
+	brake_light_material.emission_energy_multiplier = 2.85
 
 
 func enable_vehicle():
@@ -28,8 +38,8 @@ func _physics_process(delta: float):
 	if disabled:
 		return
 	
-	_steer_target = Input.get_axis(&"turn_right", &"turn_left")
-	_steer_target *= STEER_LIMIT
+	steer_target = Input.get_axis(&"turn_right", &"turn_left")
+	steer_target *= STEER_LIMIT
 
 	# Engine sound simulation
 	desired_engine_pitch = 0.05 + linear_velocity.length() / (engine_force_value * 0.5)
@@ -56,7 +66,8 @@ func _physics_process(delta: float):
 		engine_force = 0.0
 
 	# Handle braking/reversing
-	if Input.is_action_pressed(&"reverse"):
+	var reverse_pressed = Input.is_action_pressed(&"reverse")
+	if reverse_pressed:
 		var speed := linear_velocity.length()
 		if speed < 5.0 and not is_zero_approx(speed):
 			engine_force = -clampf(engine_force_value * 5.0 / speed, 0.0, 100.0)
@@ -64,6 +75,13 @@ func _physics_process(delta: float):
 			engine_force = -engine_force_value
 
 		engine_force *= Input.get_action_strength(&"reverse")
+		if not brake_lights_on and brake_light_material:
+			brake_lights_on = true
+			brake_light_material.emission_energy_multiplier = 2.85
+	else:
+		if brake_light_material:
+			brake_light_material.emission_energy_multiplier = 0
+			brake_lights_on = false
 
-	steering = move_toward(steering, _steer_target, STEER_SPEED * delta)
+	steering = move_toward(steering, steer_target, STEER_SPEED * delta)
 	previous_speed = linear_velocity.length()
