40f231badab502e3ace27b48ddbef48486344ac5
lampealex888 <lampealex888@gmail.com>
2025-07-16 17:11:37 -0400

    working on ai car collision avoidance

--- entities/vehicles/ai_car/ai_car.gd
+++ entities/vehicles/ai_car/ai_car.gd
@@ -5,13 +5,14 @@ const VEHICLES = 5
 const PLAYER_CAR = 2
 
 @export var max_speed: float = 2.0
+@export var safe_distance: float = 1.0  # Distance to maintain from other cars
+@export var check_ahead_distance: float = 3.0  # How far ahead to check on current path
 
-var new_path
+var new_path: Path3D
 var is_following_path: bool = true
 
 @onready var rigid_body = $RigidBody3D
-@onready var path_ray_cast = rigid_body.get_node("PathRayCast3D")
-@onready var traffic_ray_cast = rigid_body.get_node("TrafficRayCast3D")
+@onready var path_ray_cast = rigid_body.get_node("RayCast3D")
 @onready var despawn_timer = rigid_body.get_node("DespawnTimer")
 
 static var vehicle_paths: Array[String] = [
@@ -50,24 +51,15 @@ func _process(delta):
 	if not is_following_path:
 		return
 	
-	# Stop if traffic ahead, otherwise move at max speed
 	var speed := 0.0
-	if traffic_ray_cast.is_colliding():
-		var collider = traffic_ray_cast.get_collider()
-		if collider and collider.get_collision_layer_value(VEHICLES):
-			var my_forward = -rigid_body.global_transform.basis.z
-			var other_forward = -collider.global_transform.basis.z
-			var dot_product = my_forward.dot(other_forward)
-			if dot_product > 0:
-				speed = 0.0
-			else:
-				speed = max_speed
-		if collider and collider.get_collision_layer_value(PLAYER_CAR):
-			speed = 0.0
+	
+	# Check for traffic on current path and potential new path
+	if should_wait_for_traffic():
+		speed = 0.0
 	else:
 		speed = max_speed
 	
-	# Check for path transmitter
+	# Check for path transmitter for potential path changes
 	if path_ray_cast.is_colliding():
 		var transmitter = path_ray_cast.get_collider()
 		if transmitter and transmitter.get_collision_layer_value(PATH_TRANSMITTER):
@@ -80,11 +72,65 @@ func _process(delta):
 	if progress_ratio >= 1.0:
 		_switch_to_new_path()
 
+func should_wait_for_traffic() -> bool:
+	# Check current path for cars ahead
+	if is_car_ahead_on_current_path():
+		return true
+	
+	# Check if we're about to switch paths and if the new path has traffic
+	if new_path and is_instance_valid(new_path) and progress_ratio > 0.8:
+		return is_traffic_on_path(new_path)
+	
+	return false
+
+func is_car_ahead_on_current_path() -> bool:
+	var current_path = get_parent()
+	if not current_path is Path3D:
+		return false
+	
+	# Get all cars on the current path
+	var cars_on_path = current_path.get_children().filter(func(child): return child != self and child.has_method("_process"))
+	
+	for car in cars_on_path:
+		if car.progress > progress and car.progress - progress < check_ahead_distance:
+			return true
+	
+	return false
+
+func is_traffic_on_path(path: Path3D) -> bool:
+	if not path or not is_instance_valid(path):
+		return false
+	
+	# Get all cars on the target path
+	var cars_on_path = path.get_children().filter(func(child): return child.has_method("_process"))
+	
+	# Check if there are any cars near the beginning of the path
+	for car in cars_on_path:
+		if car.progress < safe_distance:
+			return true
+	
+	return false
+
+func can_safely_enter_path(path: Path3D) -> bool:
+	if not path or not is_instance_valid(path):
+		return false
+	
+	# More sophisticated check - ensure there's enough space at the beginning
+	var cars_on_path = path.get_children().filter(func(child): return child.has_method("_process"))
+	
+	for car in cars_on_path:
+		# Check if any car is too close to the start of the path
+		if car.progress < safe_distance * 2:
+			return false
+	
+	return true
+
 func _switch_to_new_path():
 	get_parent().remove_child(self)
-	if new_path and is_instance_valid(new_path):
+	if new_path and is_instance_valid(new_path) and can_safely_enter_path(new_path):
 		new_path.add_child(self)
 		progress = 0.0
+		new_path = null  # Clear the new path reference
 	else:
 		is_following_path = false
 		despawn_timer.start()
@@ -95,7 +141,6 @@ func on_player_collision(body):
 		despawn_timer.start()
 		call_deferred("handle_collision_cleanup")
 		path_ray_cast.enabled = false
-		traffic_ray_cast.enabled = false
 
 func handle_collision_cleanup():
 	var pos = rigid_body.global_position
